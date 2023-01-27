2da7cc1516ddd8896e5fb75035dca4ea6d6caafe
Duc Vu <vuduc1812@gmail.com>
2023-01-27 01:20:57 +0000

    Add notification scheduling for task

--- src/screens/Task.js
+++ src/screens/Task.js
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Alert, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, {useEffect, useState} from 'react'
 import { TextInput } from 'react-native-gesture-handler'
 import CustomButton from '../utils/CustomButton'
@@ -7,6 +7,7 @@ import { setTasks } from '../redux/actions';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import CheckBox from '@react-native-community/checkbox';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import PushNotification from 'react-native-push-notification';
 
 export default function Task({navigation}) {
 
@@ -16,7 +17,9 @@ export default function Task({navigation}) {
     const [title, setTitle] = useState("");
     const [desc, setDesc] = useState("");
     const [done, setDone] = useState(false);
+    const [showBellModal, setShowBellModal] = useState(false);
     const [color, setColor] = useState("white");
+    const [bellTime, setBellTime] = useState("1");
 
     useEffect(() => {
         getTask();
@@ -66,8 +69,59 @@ export default function Task({navigation}) {
         }
     }
 
+    const setTaskAlarm = () => {
+        PushNotification.localNotificationSchedule({
+            channelId: "task-channel",
+            title: title,
+            message: desc,
+            date: new Date(Date.now() + parseInt(bellTime)*60*1000),
+            allowWhileIdle: true,
+        })
+    }
+
     return (
     <View style={styles.body}>
+        <Modal
+            visible={showBellModal}
+            transparent
+            onRequestClose={() => setShowBellModal(false)}
+            animationType="slide"
+            hardwareAccelerated
+        >
+            <View style={styles.centered_view}>
+                <View style={styles.bell_modal}>
+                    <View style={styles.bell_body}>
+                        <Text style={styles.text}>Remind me after</Text>
+                        <TextInput
+                            style={styles.bell_input}
+                            keyboardType="numeric"
+                            value={bellTime}
+                            onChangeText={(value) => setBellTime(value)}
+                        />
+                        <Text style={styles.text}>minute(s)</Text>
+                    </View>
+                    <View style={styles.bell_buttons}>
+                        <TouchableOpacity
+                            style={styles.bell_cancel_button}
+                            onPress={()=> {
+                                setShowBellModal(false)
+                            }}
+                        >
+                            <Text style={styles.text}>Cancel</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.bell_ok_button}
+                            onPress={()=> {
+                                setShowBellModal(false)
+                                setTaskAlarm()
+                            }}
+                        >
+                            <Text style={styles.text}>OK</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </Modal>
         <TextInput
             value={title}
             style={styles.input}
@@ -129,6 +183,18 @@ export default function Task({navigation}) {
                         color={"#000000"}
                     />
                 }
+            </TouchableOpacity>
+        </View>
+        <View style={styles.extra_row}>
+            <TouchableOpacity
+                style={styles.extra_button}
+                onPress={() => {setShowBellModal(true)}}
+            >
+                <FontAwesome5
+                    name={"bell"}
+                    size={25}
+                    color={"#ffffff"}
+                />
             </TouchableOpacity>
         </View>
         <View style={styles.checkbox}>
@@ -210,5 +276,67 @@ const styles = StyleSheet.create({
         alignItems: "center",
         borderTopRightRadius: 10,
         borderBottomRightRadius: 10,
+    },
+    extra_row: {
+        flexDirection: "row",
+        marginVertical: 10,
+    },
+    extra_button: {
+        flex: 5,
+        height: 50,
+        backgroundColor: "#0080ff",
+        borderRadius: 10,
+        marginHorizontal: 5,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    centered_view: {
+        flex: 1,
+        backgroundColor: "#00000099",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    bell_modal: {
+        width: 300,
+        height: 200,
+        backgroundColor: "#ffffff",
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: "#000000",
+    },
+    bell_body: {
+        height: 150,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    bell_buttons: {
+        flexDirection: "row",
+        height: 50,
+    },
+    bell_input: {
+        width: 50,
+        borderWidth: 1,
+        borderColor: "#555555",
+        borderRadius: 10,
+        backgroundColor: "#ffffff",
+        textAlign: "center",
+        fontSize: 20,
+        margin: 10,
+    },
+    bell_cancel_button: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: "#000000",
+        borderBottomLeftRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    bell_ok_button: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: "#000000",
+        borderBottomRightRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
     },
 })

6facdea37d22b7675c3ba67396ecb4e1171623c4
Duc Vu <vuduc1812@gmail.com>
2023-01-27 00:26:18 +0000

    Add colour strip to list view

--- src/screens/ToDo.js
+++ src/screens/ToDo.js
@@ -65,6 +65,16 @@ export default function ToDo({navigation}) {
                         }}
                     >
                         <View style={styles.item_row}>
+                            <View
+                                style={[
+                                    {
+                                        backgroundColor:
+                                            item.Color === "red" ? "#f28b82":
+                                                item.Color === "blue" ? "#aecbfa":
+                                                    item.Color === "green" ? "#ccff90": "#ffffff"
+                                    },
+                                    styles.color]}
+                            />
                             <CheckBox
                                 value={item.Done}
                                 onValueChange={(NewValue) => {checkTask(item.ID, NewValue)}}
@@ -153,7 +163,7 @@ const styles = StyleSheet.create({
     item: {
         marginHorizontal: 10,
         marginVertical: 7,
-        paddingHorizontal: 10,
+        paddingRight: 10,
         backgroundColor: "#ffffff",
         justifyContent: "center",
         borderRadius: 10,
@@ -168,5 +178,11 @@ const styles = StyleSheet.create({
         color: "#999999",
         fontSize: 20,
         margin: 5,
+    },
+    color: {
+        width: 20,
+        height: 100,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
     }
 })
